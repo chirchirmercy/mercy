e21fdbf0c3a3b86412350e19172d7f64d11d0c38
chirchirmercy <cmercychptoo.com>
2023-02-14 07:38:57 +0300

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,25 @@
-fun main (args: Array<String>){
-    greetperson("mercy")
-    remainder(34,5)
-    var sum=addition(num1 = 34, num2 =56, num3 = 57, num4 = 60)
-    println(sum)
-    var String=("am honest")
-    println(String)
-}
-
-
-fun greetperson(name: String){
-    println("Hello $name" )
-}
-fun remainder(num1:Int, num2:Int):Int{
-    var remainder=num1%num2
+fun main(){
+greeting("john")
+    var remainder=remainder(100,80)
     println(remainder)
-    return(remainder)
+   var sum= sum(200,42,56,87)
+    println(sum)
+    aboutMe("Cooking bananas.")
 }
-fun addition(num1:Int, num2:Int, num3:Int, num4:Int):Int{
-    var sum= num1+num2+num3+num4
-    return(sum)
+fun greeting(name:String){
+    println("Hello  $name")
 }
-fun statement(Mercy:String):String{
-    return(Mercy)
+fun remainder(num1:Int, num2:Int) : Int{
+    var remainder=num1%num2
+    return remainder
 }
-
-
+fun sum(num1:Int, num2:Int, num3:Int, num4:Int) : Int{
+    var sum=num1+num2+num3+num4
+    return sum
+}
+fun aboutMe(fact:String){
+    println("I like $fact")
+}
 
 
 
